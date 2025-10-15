e128ab142bdd7d50e3d6f781cba5cbedda4cfa2f
aaanhho <hovuphuonganhh@gmail.com>
2025-10-15 12:43:43 +1100

    allow CORS so backend able to communicate with frontend

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=localshop.db"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalFrontend", policy =>
+        policy.WithOrigins("http://localhost:3000") 
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -18,5 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowLocalFrontend");
+
 app.MapControllers();
 app.Run();
